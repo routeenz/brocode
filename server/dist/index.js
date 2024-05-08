1e75f46aab8a7eee2e758f5909ec2dac110621a4
routeenz <madhavankuruva@gmail.com>
2024-05-08 23:25:13 +0530

    deploy changes

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
@@ -13,11 +15,13 @@ const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
-app.use((0, cors_1.default)({ credentials: true, origin: "http://localhost:5173" }));
+app.use(
+  (0, cors_1.default)({ credentials: true, origin: "http://localhost:4173" })
+);
 (0, dotenv_1.config)();
 app.use("/compiler", compilerRouter_1.compilerRouter);
 app.use("/user", userRouter_1.userRouter);
 (0, dbConnect_1.dbConnect)();
 app.listen(4000, () => {
-    console.log("http://localhost:4000");
+  console.log("http://localhost:4000");
 });
